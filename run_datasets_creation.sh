12a18c945c1de6c937053b4ea3f318338beef33a
SeitaroShinagawa <sei.shinagawa@is.naist.jp>
2022-04-28 17:17:33 +0900

    modified

--- run_datasets_creation.sh
+++ run_datasets_creation.sh
@@ -4,8 +4,17 @@ mkdir data/search/gr
 mkdir data/search/ml25m
 mkdir data/dialogue/books
 mkdir data/dialogue/movies
+mkdir data/dialogue/music
 
 cd data/
-sbatch create_dialogue_datasets.sbatch
-sbatch create_recommendation_datasets.sbatch
-sbatch create_search_datasets.sbatch
+#sbatch create_dialogue_datasets.sbatch
+#sbatch create_recommendation_datasets.sbatch
+#sbatch create_search_datasets.sbatch
+echo "create_dialogue_datasets.sh"
+bash create_dialogue_datasets.sh
+
+echo "create_recommendation_datasets.sh"
+bash create_recommendation_datasets.sh
+
+echo "create_search_datasets.sh"
+bash create_search_datasets.sh
